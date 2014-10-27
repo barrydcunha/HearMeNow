79e24204a7beb4bda1a6556fcc9c0484d55a37c9
Barry DCunha <barry@Susanta-Mohapatras-MacBook-Pro.local>
2014-10-27 14:54:49 +0530

    Initial commit

---
+++ HearMeNow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HearMeNow
+//
+//  Created by Barry DCunha on 27/10/14.
+//  Copyright (c) 2014 OctoberApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HearMeNow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HearMeNow
+//
+//  Created by Barry DCunha on 27/10/14.
+//  Copyright (c) 2014 OctoberApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
